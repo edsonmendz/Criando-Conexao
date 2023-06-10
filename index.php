1e3b8ba02d2ad49386bfa3f0c342a1af6fcd8609
edsonmendz <japaact@gmail.com>
2023-06-10 09:59:51 -0300

    Queries de inserção

--- index.php
+++ index.php
@@ -7,7 +7,39 @@
     try {
         $conexao = new PDO($dsn, $usuario, $senha );
 
-    } catch(PDOException $e) {
-        echo 'Erro: ' . $e->getCode() . 'Mensagem: ' . $e->getMessage();
+        /*
+        $query = '
+        create table if not exists tb_usuarios (
+            id int not null primary key auto_increment,
+            nome varchar(50) not null,
+            email varchar(100) not null,
+            senha varchar(32) not null
+        )
+        ';
 
+        $retorno = $conexao->exec($query);
+        */
+
+        
+
+        $inserir = '
+         insert into tb_usuarios( nome, email, senha) values ( "Jamilton", "jamilton@teste.com.br", "abcde"
+         ) 
+        ';
+
+        $conexao->exec($inserir);
+
+        $inserir = '
+         insert into tb_usuarios( nome, email, senha) values ( "Mariazinha", "maria@teste.com.br", "2345678"
+         ) 
+        ';
+
+        $conexao->exec($inserir);
+
+
+
+    } catch(PDOException $e) {
+
+        echo 'Erro: ' . $e->getCode() . 'Mensagem: ' . $e->getMessage();
+        
     } 
